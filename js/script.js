429951dd2196745fdab61f85470ee71914b2254b
damien-simplon <damien.bellec12@gmail.com>
2021-10-25 13:19:37 +0200

    fetch GET effectué sur l'index.html

--- js/script.js
+++ js/script.js
@@ -1,13 +1,29 @@
 var url = 'https://reqres.in/api/users?';
-fetch(url + new URLSearchParams({
-    per_page : 12
-})).then((res) => {
+var body = document.getElementById('main');
+
+fetch(
+	url +
+		new URLSearchParams({
+			per_page: 12,
+		}),
+	{
+		method: 'GET',
+	}
+).then((res) => {
 	console.log(res);
 	if (res.ok) {
 		res.json().then((data) => {
-			data.data.forEach(element => {
-                console.log(element);
-            });
+			data.data.forEach((element) => {
+				console.log(element);
+				var html = `
+                    <div class="card">
+                        <h2>${element.first_name} ${element.last_name}</h2>
+                        <img src="${element.avatar}" alt="Avatar de ${element.first_name} ${element.last_name}"/>
+                        <h3><a href="mailto:${element.email}">${element.email}</a></h3>
+                    </div>
+                `;
+				body.insertAdjacentHTML('beforeend', html);
+			});
 		});
 	} else {
 		console.log("Erreur du chargement de l'API");
