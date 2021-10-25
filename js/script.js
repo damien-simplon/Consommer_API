7a89af9f3fd457d6b1fef4fc3db48b82a7f13a8f
damien-simplon <damien.bellec12@gmail.com>
2021-10-25 11:40:44 +0200

    commencement du get

--- js/script.js
+++ js/script.js
@@ -1,28 +1,15 @@
-/*
-const img = document.getElementById('img');
-
-fetch('https://api.thecatapi.com/v1/images/search')
-	.then((res) => {
-        console.log(res);
-        if(res.ok){
-            res.json().then(data => {
-                img.src = data[0].url
-            })
-        }else{
-            document.getElementById('erreur').innerHTML = "Erreur du chargement de l'API";
-        }
-    })
-
-*/
-
-function updatemenu() {
-    if (document.getElementById('responsive-menu').checked == true) {
-      document.getElementById('menu').style.borderBottomRightRadius = '0';
-      document.getElementById('menu').style.borderBottomLeftRadius = '0';
-    }else{
-      document.getElementById('menu').style.borderRadius = '10px';
-    }
-  }
-
-fetch('https://reqres.in/api/users')
-.then(res => console.log(res.json()))
+var url = 'https://reqres.in/api/users?';
+fetch(url + new URLSearchParams({
+    per_page : 12
+})).then((res) => {
+	console.log(res);
+	if (res.ok) {
+		res.json().then((data) => {
+			data.data.forEach(element => {
+                console.log(element);
+            });
+		});
+	} else {
+		console.log("Erreur du chargement de l'API");
+	}
+});
